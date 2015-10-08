f1d9cbb4d0db4818897b47338e91c53e468ea0d5
Orleika <orleika.net@gmail.com>
2015-10-08 13:27:39 +0900

    Update domain

--- bin/install.sh
+++ bin/install.sh
@@ -6,4 +6,4 @@ cd /var/www
 forever stop server.js
 sudo cp -Rf $HOME/turbo_boost_detection/* /var/www/
 sudo npm i
-NODE_ENV=production HOST=`hostname`.gq MONGODB_URI=mongodb://localhost/turbo forever start server.js
+NODE_ENV=production HOST=`hostname`.ga MONGODB_URI=mongodb://localhost/turbo forever start server.js
